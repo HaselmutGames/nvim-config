2267b2088af96700144b756e13921c8b4e52dd10
HaselmutGames <haselmut.games@gmail.com>
2025-10-14 16:24:28 +0200

    Update telescope desc

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find in [P]roject [f]iles' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find in a git repo' })
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
