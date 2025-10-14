41d13f3f823b4cd66af76d634f89e4902de37c09
HaselmutGames <haselmut.games@gmail.com>
2025-10-14 16:08:36 +0200

    Install plugin to show pending keymaps

--- lua/haselmut/packer.lua
+++ lua/haselmut/packer.lua
@@ -27,6 +27,24 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  -- Plugin to show pending keybinds
+  use({
+      'folke/which-key.nvim',
+      event = 'VimEnter',
+      opts = {
+          delay = 0,
+          icons = {
+              mappings = vim.g.have_nerd_font,
+              keys = vim.g.have_nerd_font and {},
+          },
+          -- Document existing key chains
+          spec = {
+              { '<leader>s', group = '[S]earch' },
+              { '<leader>t', group = '[T]oggle' },
+              { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+          },
+      },
+  })
 
   -- LSP
   use{
