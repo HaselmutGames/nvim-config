38e87ad2e339720325f0b17143e368e4fd7b46a8
HaselmutGames <haselmut.games@gmail.com>
2025-10-14 23:35:52 +0200

    format and indent file

--- lua/haselmut/packer.lua
+++ lua/haselmut/packer.lua
@@ -4,96 +4,96 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine-moon')
-	  end
-  })
-  use{'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons'}
-  }
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  -- Plugin to show pending keybinds
-  use({
-      'folke/which-key.nvim',
-      event = 'VimEnter',
-      opts = {
-          delay = 0,
-          icons = {
-              mappings = vim.g.have_nerd_font,
-              keys = vim.g.have_nerd_font and {},
-          },
-          -- Document existing key chains
-          spec = {
-              { '<leader>s', group = '[S]earch' },
-              { '<leader>t', group = '[T]oggle' },
-              { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-          },
-      },
-  })
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine-moon')
+        end
+    })
+    use{'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons'}
+}
+use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use('nvim-treesitter/playground')
+use('theprimeagen/harpoon')
+use('mbbill/undotree')
+use('tpope/vim-fugitive')
+-- Plugin to show pending keybinds
+use({
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    opts = {
+        delay = 0,
+        icons = {
+            mappings = vim.g.have_nerd_font,
+            keys = vim.g.have_nerd_font and {},
+        },
+        -- Document existing key chains
+        spec = {
+            { '<leader>s', group = '[S]earch' },
+            { '<leader>t', group = '[T]oggle' },
+            { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        },
+    },
+})
 
-  -- LSP plugins
-  use({
-      -- 'lazydev configures Lua LSP for Neovim config, runtime and plugins
-      -- used for completion, annotations, and signatures of Neovim apis
-      'folke/lazydev.nvim',
-      ft = 'lua',
-      opts = {
-          library = {
-              -- Load luvit types when the 'vim.uv' word is found
-              { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-          },
-      },
-  })
-  -- Main LSP configuration
-  use ({
-      'neovim/nvim-lspconfig',             -- core LSP client configurations
-      requires = {
-          'williamboman/mason.nvim',
-          'williamboman/mason-lspconfig.nvim',
-          'WhoIsSethDaniel/mason-tool-installer.nvim',
-          'j-hui/fidget.nvim',
-      },
-      config = function()
-          require('mason').setup()
-          require('mason-lspconfig').setup()
-        end,
-  })
-  use({
-      'hrsh7th/nvim-cmp',
-      requires = {
-          'hrsh7th/cmp-nvim-lsp',
-          'hrsh7th/cmp-buffer',
-          'hrsh7th/cmp-path',
-          'hrsh7th/cmp-cmdline',
-          'L3MON4D3/LuaSnip',
-      },
-      config = function()
-          local cmp = require('cmp')
-          cmp.setup({
-              mapping = cmp.mapping.preset.insert({
-                  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-              }),
-              sources = cmp.config.sources({
-                  { name = 'nvim_lsp' },
-              }, {
-                  { name = 'buffer' },
-              }),
-          })
-      end,
-  })
+-- LSP plugins
+use({
+    -- 'lazydev configures Lua LSP for Neovim config, runtime and plugins
+    -- used for completion, annotations, and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+        library = {
+            -- Load luvit types when the 'vim.uv' word is found
+            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        },
+    },
+})
+-- Main LSP configuration
+use ({
+    'neovim/nvim-lspconfig',             -- core LSP client configurations
+    requires = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+        'j-hui/fidget.nvim',
+    },
+    config = function()
+        require('mason').setup()
+        require('mason-lspconfig').setup()
+    end,
+})
+use({
+    'hrsh7th/nvim-cmp',
+    requires = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'L3MON4D3/LuaSnip',
+    },
+    config = function()
+        local cmp = require('cmp')
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+            }, {
+                { name = 'buffer' },
+            }),
+        })
+    end,
+})
 end)
