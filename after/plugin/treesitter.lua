7fd5011a762fc7e2c67d4c376c0e6dc897e3773c
HaselmutGames <haselmut.games@gmail.com>
2025-10-17 23:38:32 +0200

    Add new langs to treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	ensure_installed = { "c", "lua", "vim",  "javascript", "typescript", "c_sharp", "java", "python" },
+        ensure_installed = { "c", "c_sharp", "cpp", "lua", "vim", "vimdoc", "javascript", "typescript", "css", "html", "java", "python", "bash", "tmux" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
