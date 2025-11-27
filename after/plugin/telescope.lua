c7068de33691b08e7b35c1994e1a904436b904fc
HaselmutGames <haselmut.games@gmail.com>
2025-11-27 18:13:12 +0100

    Add diagnostics keymaps to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,3 +10,10 @@ vim.keymap.set('n', '<leader>sn', function()
     builtin.find_files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[S]earch [N]eovim files' })
 
+-- Show all workspace diagnostics
+vim.keymap.set('n', '<leader>lw', builtin.diagnostics, { desc = '[L]ist [W]orkspace Diagnostics' })
+
+-- Show only diagnostics for current buffer
+vim.keymap.set('n', '<leader>lb', function()
+    builtin.diagnostics({ bufnr = 0 })
+end, { desc = '[L]ist [B]uffer Diagnostics' })
